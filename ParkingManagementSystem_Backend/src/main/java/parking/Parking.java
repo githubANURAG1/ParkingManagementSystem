e22e94facb33589e659629026d8eaf43c4ec1792
githubANURAG1 <anurag0429rai@gmail.com>
2025-06-15 01:12:11 +0530

    changes

--- ParkingManagementSystem_Backend/src/main/java/parking/Parking.java
+++ ParkingManagementSystem_Backend/src/main/java/parking/Parking.java
@@ -2,6 +2,7 @@ package parking;
 
 public interface Parking {
     int parkingCharger = 0;
-    parkingDetailsDTO checkparking(int parkingId);
+    void parkingDetailsDTO checkparking(int parkingId);
+
 
 }
